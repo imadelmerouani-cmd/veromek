efb46fb3e4071ce38b00e731a510ceeed6f57052
imadelmerouani-cmd <imadelmeouani@gmail.com>
2026-07-31 04:30:24 +0100

    Deploy complete homepage and delivery proofs

--- veromek/src/pages/Home.jsx
+++ veromek/src/pages/Home.jsx
@@ -1,14 +1,26 @@
 import Layout from "../components/layout/Layout";
 import Hero from "../components/Hero";
-import Categories from "../components/Categories";
+import TrustBar from "../components/TrustBar";
 import FeaturedProducts from "../components/FeaturedProducts";
+import DeliveryProof from "../components/DeliveryProof";
+import Categories from "../components/Categories";
+import WhyChooseUs from "../components/WhyChooseUs";
+import SocialProof from "../components/SocialProof";
+import FAQ from "../components/FAQ";
+import FinalCTA from "../components/FinalCTA";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
-      <Categories />
+      <TrustBar />
       <FeaturedProducts />
+      <DeliveryProof />
+      <Categories />
+      <WhyChooseUs />
+      <SocialProof />
+      <FAQ />
+      <FinalCTA />
     </Layout>
   );
 }
